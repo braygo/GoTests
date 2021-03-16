aef221fc9304c6822010515c552f890322aab9e3
braymuk <braydeng9@gmail.com>
2021-03-16 16:15:39 -0700

    Completed iteration package

---
+++ iteration/repeat_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q, got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 4)
+	}
+}
+
+func ExampleRepeat() {
+	result := Repeat("a", 6)
+	fmt.Println(result)
+	//Output: aaaaaa
+}
