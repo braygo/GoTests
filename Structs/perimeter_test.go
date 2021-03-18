34c46fd2af44e2fa2815d9c28767c8f0092e83bb
braymuk <braydeng9@gmail.com>
2021-03-17 20:00:11 -0700

    Introduced interfaces

--- Structs/perimeter_test.go
+++ Structs/perimeter_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"math"
 	"testing"
 )
 
@@ -16,23 +15,20 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 2.0}
-		got := rectangle.Area()
-		want := 24.0
-
-		if want != got {
-			t.Errorf("got %.2f, want %.2f", got, want)
-		}
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := math.Pi * 100
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 2.0}
+		checkArea(t, rectangle, 24.0)
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
 	})
 
 }
