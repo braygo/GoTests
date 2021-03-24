92db7796c184d9d0b72baf6b4ea83afd525a5f92
braymuk <braydeng9@gmail.com>
2021-03-23 20:13:04 -0700

    Refactored code, now passing second or third test

--- Select/WebsiteRacer_test.go
+++ Select/WebsiteRacer_test.go
@@ -1,10 +1,21 @@
 package Select
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowUrl := "http://www.facebook.com"
-	fastUrl := "http://www.quii.co.uk"
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowUrl := slowServer.URL
+	fastUrl := fastServer.URL
 
 	want := fastUrl
 	got := Racer(slowUrl, fastUrl)
@@ -12,4 +23,14 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
