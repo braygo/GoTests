1e7b8dfd7184fd2fa93273a2d98e34deb0048d00
braymuk <braydeng9@gmail.com>
2021-04-04 21:29:19 -0700

    This section is really janky, might just come back to this later

---
+++ Maths/clock_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"math"
+	"testing"
+	"time"
+
+	"github.com/quii/learn-go-with-tests/math/v1/clockface"
+)
+
+func TestClock(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
+
+	if want != got {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := clockface.Point{X: 150, Y: 150 + 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestSecondsInRadians(t *testing.T) {
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := secondsInRadians(thirtySeconds)
+
+	if got != want {
+		t.Fatalf("Wanted %v radians, but got %v", want, got)
+	}
+}
