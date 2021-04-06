cf989196c00303e73126c23426802d8cb87893e5
braymuk <braydeng9@gmail.com>
2021-04-06 15:39:05 -0700

    Some refactoring

--- Application/server.go
+++ Application/server.go
@@ -3,8 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, GetPlayerScore(player))
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+	if name == "Floyd" {
+		return "10"
+	}
+	return ""
 }
